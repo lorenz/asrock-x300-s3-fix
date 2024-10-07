686917d14028f393f48b50be8cfd6484858b1c27
Lorenz Brun <lorenz@brun.one>
2024-10-07 22:18:33 +0200

    Support multiple BIOS versions
    Use sed to set the DSDT OEM revision independent of what it was on the
    original table. This allows the fix to work with many more BIOS
    versions.

--- mkoverride.sh
+++ mkoverride.sh
@@ -8,6 +8,8 @@ cp *.aml $TMPDIR/
 cd $TMPDIR
 iasl -e ssdt1.aml -e ssdt6.aml -d dsdt.aml
 for i in $SELF_DIR/dsdt/*.patch; do patch -p1 < $i; done
+# Bump OEM revision so Linux picks it up over the built-in table
+sed -i 's/\(DefinitionBlock \(.*\), \)\(.*\)/\10x0FFFFFFF)/' dsdt.dsl
 iasl -tc dsdt.dsl
 gen_init_cpio $SELF_DIR/override_cpio_spec > $ORIG_PWD/acpi_dsdt_override.cpio
 cd / && rm -Rf $TMPDIR
